0adb8b5e154829c08f86f8620b7be4a4779bb548
Brian Hanquet <apps.brinat@pm.me>
2024-07-06 22:15:01 +0200

    Add multi threading

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+use hola::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    path::Path,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let available_parallelism = thread::available_parallelism().unwrap();
+    println!("Starting with {available_parallelism} thread");
+    let pool = ThreadPool::new(available_parallelism);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
+        pool.execute(|| {
         handle_connection(stream);
+        });
 
     }
 }
@@ -21,9 +29,9 @@ fn handle_connection(mut stream: TcpStream) {
 
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "html\\index.html")
+        ("HTTP/1.1 200 OK", Path::new("html").join("index.html"))
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "html\\404.html")
+        ("HTTP/1.1 404 NOT FOUND", Path::new("html").join("404.html"))
     };
 
     let contents = fs::read_to_string(filename).unwrap();
